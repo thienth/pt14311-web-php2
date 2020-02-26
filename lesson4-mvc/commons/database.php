74b60bc74865c03bdd0d3eec1737cad7e477c89d
thienth32 <thienth32@gmail.com>
2020-02-26 12:17:01 +0700

    update

--- lesson4-mvc/commons/database.php
+++ lesson4-mvc/commons/database.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: ginv2
+ * Date: 2/26/20
+ * Time: 12:15
+ */
+
 use Illuminate\Database\Capsule\Manager as Capsule;
+
 $capsule = new Capsule;
+
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => '127.0.0.1',
+    'host'      => 'localhost',
     'database'  => 'pt14311-web',
     'username'  => 'root',
     'password'  => '123456',
@@ -11,8 +20,14 @@ $capsule->addConnection([
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 $capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
 $capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
